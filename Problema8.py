f0b323cf069c68359d1e2a5b4ab03e25160d8089
JohnRojas420 <luisenrique.rojas@unmsm.edu.pe>
2024-07-30 02:25:01 +0000

    Subiendo el problema 8

---
+++ Problema8.py
@@ -0,0 +1,43 @@
+# Cree un módulo llamado operaciones.py el cual contendrá 4 funciones para realizar una suma,una resta, un producto
+# y una división entre dos números. Todas ellas devolverán el resultado. En las funciones del módulo deberá de 
+# haber tratamiento e invocación manual de errores para evitar que se quede bloqueada una funcionalidad, esto
+# incluye:
+# ● En caso de que se envíen valores a las funciones que no sean números, deberá aparecer un mensaje que informe
+#   Error: Tipo de dato no válido.
+# ● En caso de realizar una división por cero, deberá aparecer un mensaje que informe Error: No es posible
+#  dividir entre cero.
+# Una vez creado el módulo, crea un script calculos.py en el mismo directorio en el que deberásimportar el 
+# módulo y ejecutar las funciones.
+
+#Solución:
+
+# operaciones.py
+
+def suma(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return "Error: Tipo de dato no válido."
+
+def resta(a, b):
+    try:
+        return a - b
+    except TypeError:
+        return "Error: Tipo de dato no válido."
+
+def producto(a, b):
+    try:
+        return a * b
+    except TypeError:
+        return "Error: Tipo de dato no válido."
+
+def division(a, b):
+    try:
+        if b == 0:
+            return "Error: No es posible dividir entre cero."
+        return a / b
+    except TypeError:
+        return "Error: Tipo de dato no válido." 
+
+
+
